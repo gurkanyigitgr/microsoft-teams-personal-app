9a09c1cc768bbe2b59e714cb6b6d50285f507b4c
Gürkan <gurkanyigitgr@gmail.com>
2023-11-24 01:55:29 +0300

    add themeSlice for dark and light mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import triviaReducer from './features/triviaSlice';
 import bookReducer from './features/bookSlice';
 import navigationReducer from './features/navigationSlice';
 import coinReducer from './features/coinSlice';
+import themeReducer from './features/themeSlice';
 const store = configureStore({
   reducer: {
     trivia: triviaReducer,
     book: bookReducer,
     navigation: navigationReducer,
     coins: coinReducer,
+    theme: themeReducer,
   },
 });
 export default store;
