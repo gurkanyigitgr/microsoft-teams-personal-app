08a012edc9ceef318856bb48e2481a7dc2e6e7ea
Gürkan <gurkanyigitgr@gmail.com>
2023-11-23 06:54:12 +0300

    add bookcontent, coincontent, triviacontent

---
+++ src/components/Coin/CoinContent.jsx
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import { useSelector } from 'react-redux';
+import {
+  makeStyles,
+  shorthands,
+  Button,
+  Caption1,
+  Text,
+  tokens,
+  Subtitle1,
+} from '@fluentui/react-components';
+import { Card, CardHeader } from '@fluentui/react-components';
+
+const useStyles = makeStyles({
+  card: {
+    width: '360px',
+    maxWidth: '100%',
+    maxHeight: '160px',
+    cursor: 'pointer',
+    boxShadow: '6px 8px 4px rgba(0, 0, 0, 0.25)',
+    backgroundColor: '#272829',
+  },
+
+  headerImage: {
+    ...shorthands.borderRadius('4px'),
+    maxWidth: '40px',
+    maxHeight: '25px',
+  },
+  text: {
+    ...shorthands.margin(0),
+    fontSize: '12px',
+  },
+});
+
+export default function CoinContent() {
+  const styles = useStyles();
+  const coins = useSelector((state) => state.coins.coins.coins);
+  const nfts = useSelector((state) => state.coins.nfts.nfts);
+
+  return (
+    <div className='flex gap-5'>
+      <div className='flex flex-col items-center'>
+        <h2 className='text-4xl font-bold mb-16'>Coins</h2>
+        <div className='md:grid md:grid-cols-2 gap-5'>
+          {coins.map((coin, index) => (
+            <Card key={index} className={styles.card} s>
+              <CardHeader
+                image={
+                  <img
+                    className={styles.headerImage}
+                    src={coin.item.thumb}
+                    alt='App Name Document'
+                  />
+                }
+                header={
+                  <Text
+                    style={{ fontSize: '15px', color: '#fff' }}
+                    weight='semibold'
+                  >
+                    {coin.item.name}
+                  </Text>
+                }
+              />
+
+              <p className={styles.text} style={{ color: '#fff' }}>
+                Donut chocolate bar oat cake. Dragée tiramisu lollipop bear
+                claw. Marshmallow pastry jujubes toffee sugar plum.
+              </p>
+            </Card>
+          ))}
+        </div>
+      </div>
+      <div className='flex flex-col items-center'>
+        <h2 className='text-4xl font-bold mb-16'>NFTs</h2>
+        <div className='md:grid md:grid-cols-2 gap-5'>
+          {nfts.map((nft, index) => (
+            <Card key={index} className={styles.card}>
+              <CardHeader
+                image={
+                  <img
+                    className={styles.headerImage}
+                    src={nft.thumb}
+                    alt='App Name Document'
+                  />
+                }
+                header={
+                  <Text
+                    style={{ fontSize: '15px', color: '#fff' }}
+                    weight='semibold'
+                  >
+                    {nft.name}
+                  </Text>
+                }
+              />
+
+              <p className={styles.text} style={{ color: '#fff' }}>
+                Donut chocolate bar oat cake. Dragée tiramisu lollipop bear
+                claw. Marshmallow pastry jujubes toffee sugar plum.
+              </p>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Trivia/TriviaContent.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  Dialog,
+  DialogSurface,
+  DialogTitle,
+  DialogBody,
+  DialogActions,
+  DialogContent,
+  Button,
+} from '@fluentui/react-components';
+
+export default function TriviaContent() {
+  const style = {
+    backgroundColor: '#e5e7eb',
+    color: 'black',
+    border: 'none',
+    borderRadius: '5px',
+  };
+  const triviaCategories = useSelector((state) => state.trivia.categories);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    setIsDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setIsDialogOpen(false);
+  };
+
+  return (
+    <div className='flex flex-col items-center w-full h-full'>
+      <h1 className='text-4xl font-bold mb-8 text-[#000]'>Trivia Categories</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {triviaCategories.map((category) => (
+          <div
+            key={category.id}
+            className='bg-[#272829] p-3.5 rounded-xl shadow-md text-white cursor-pointer hover:scale-105 duration-500 transition-all'
+            onClick={() => handleCategoryClick(category)}
+          >
+            <h2 className='text-xl font-semibold mb-2'>{category.name}</h2>
+            <p className='text-gray-300'>{category.description}</p>
+          </div>
+        ))}
+      </div>
+      {selectedCategory && (
+        <Dialog open={isDialogOpen} onClose={handleCloseDialog}>
+          <DialogSurface style={style}>
+            <DialogBody>
+              <DialogTitle style={{ fontWeight: 'bold' }}>
+                {selectedCategory.name}
+              </DialogTitle>
+              <DialogContent>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse,
+                rerum!
+              </DialogContent>
+              <DialogActions>
+                <Button
+                  style={{
+                    backgroundColor: '#000',
+                    color: '#fff',
+                    padding: '5px 10px',
+                    borderRadius: '5px',
+                  }}
+                  appearance='primary'
+                  onClick={handleCloseDialog}
+                >
+                  Close
+                </Button>
+              </DialogActions>
+            </DialogBody>
+          </DialogSurface>
+        </Dialog>
+      )}
+    </div>
+  );
+}
