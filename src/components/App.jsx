e15ea1dc4335e99995cf107fddccc4cb78a82f95
Gürkan <gurkanyigitgr@gmail.com>
2023-11-23 06:54:49 +0300

    add navigationmenu and content to app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import NavigationMenu from './Navigation/NavigationMenu';
+import Content from './content/Content';
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className='h-screen w-screen flex flex-col md:flex-row'>
+      <div className='w-full md:w-44 h-auto md:h-screen'>
+        <NavigationMenu />
+      </div>
+      <div className='container mx-auto w-full h-screen flex items-center'>
+        <Content />
+      </div>
+    </div>
+  );
 }
